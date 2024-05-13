7135fdb5c6a60fa3e1932651668b5e693b8b554d
Göksu Ayaz <goksuayaz79@gmail.com>
2024-05-13 23:41:07 +0300

    Update

--- src/main/java/com/vetapp/veterinary/business/concretes/AnimalManager.java
+++ src/main/java/com/vetapp/veterinary/business/concretes/AnimalManager.java
@@ -2,22 +2,29 @@ package com.vetapp.veterinary.business.concretes;
 
 import com.vetapp.veterinary.business.abs.AnimalService;
 import com.vetapp.veterinary.entity.Animal;
+import com.vetapp.veterinary.repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimalManager implements AnimalService {
 
+    @Autowired
+    private AnimalRepository animalRepository;
+
     @Override
     public Animal getById(Long id) {
-        return null;
     }
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        return this.animalRepository.save(animal);
     }
+
 
     @Override
     public Animal update(Animal animal) {
